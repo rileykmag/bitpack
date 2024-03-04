36d1a97c35e0356b0ab238b41578e5d7eea51626
Noah Daniels <ndaniels@mac.com>
2024-03-04 17:14:11 -0500

    Remove panics

--- src/bitpack.rs
+++ src/bitpack.rs
@@ -1,8 +1,7 @@
 use std::convert::TryInto;
 
-
 /// Returns true iff the signed value `n` fits into `width` signed bits.
-/// 
+///
 /// # Arguments:
 /// * `n`: A signed integer value
 /// * `width`: the width of a bit field
@@ -11,7 +10,7 @@ pub fn fitss(n: i64, width: u64) -> bool {
 }
 
 /// Returns true iff the unsigned value `n` fits into `width` unsigned bits.
-/// 
+///
 /// # Arguments:
 /// * `n`: An usigned integer value
 /// * `width`: the width of a bit field
@@ -19,58 +18,85 @@ pub fn fitsu(n: u64, width: u64) -> bool {
     false
 }
 
-/// Retrieve a signed value from `word`, represented by `width` bits
-/// beginning at least-significant bit `lsb`.
-/// 
-/// # Arguments:
-/// * `word`: An unsigned word
-/// * `width`: the width of a bit field
-/// * `lsb`: the least-significant bit of the bit field
+/// Retrieve a signed value from an unsigned `word`,
+/// beginning at least significant bit `lsb`
+/// and having `width` bytes.
+///
+/// # Arguments
+///
+/// * `word` - the word from which to extract a value
+/// * `width` - the number of bits in the field
+/// * `lsb` - the least-significant bit of the field
+///
+/// # Returns
+///
+/// a signed value corresponding to the 2s complement representation
+/// of the appropriate field of the `word`
+/// or `None` if the field is impossible
 pub fn gets(word: u64, width: u64, lsb: u64) -> i64 {
     0
 }
 
-/// Retrieve an unsigned value from `word`, represented by `width` bits
-/// beginning at least-significant bit `lsb`.
-/// 
-/// # Arguments:
-/// * `word`: An unsigned word
-/// * `width`: the width of a bit field
-/// * `lsb`: the least-significant bit of the bit field
+/// Retrieve a signed value from an unsigned `word`,
+/// beginning at least significant bit `lsb`
+/// and having `width` bytes.
+///
+/// # Arguments
+///
+/// * `word` - the word from which to extract a value
+/// * `width` - the number of bits in the field
+/// * `lsb` - the least-significant bit of the field
+///
+/// # Returns
+///
+/// a signed value corresponding to the 2s complement representation
+/// of the appropriate field of the `word`
+///
+/// or None
+/// if `lsb + width > 64`
+///
 pub fn getu(word: u64, width: u64, lsb: u64) -> u64 {
     0
 }
 
-/// Return a modified version of the unsigned `word`,
-/// which has been updated so that the `width` bits beginning at
-/// least-significant bit `lsb` now contain the unsigned `value`.
-/// Returns an `Option` which will be None iff the value does not fit
-/// in `width` unsigned bits.
-/// 
-/// # Arguments:
-/// * `word`: An unsigned word
-/// * `width`: the width of a bit field
-/// * `lsb`: the least-significant bit of the bit field
-/// * `value`: the unsigned value to place into that bit field
+/// Given an unsigned 64-bit `word`, and an unsigned `value`,
+/// pack that `value` into `width` bits of the `word` starting at
+/// least-significant bit `lsb`, if possible.
+///
+/// # Arguments
+///
+/// * `word` - an arbitrary unsigned 64-bit word
+/// * `width` - a number of bits describing a field
+/// * `lsb` - the-least significant bit of a field
+/// * `value` - the unsigned value to store in the field
+///
+/// # Returns
+///
+/// an `Option<u64>` which contains the desired value at the appropriate field, if possible
+/// If the value does not fit, returns `None`
 pub fn newu(word: u64, width: u64, lsb: u64, value: u64) -> Option<u64> {
     Some(0)
 }
 
-/// Return a modified version of the unsigned `word`,
-/// which has been updated so that the `width` bits beginning at
-/// least-significant bit `lsb` now contain the signed `value`.
-/// Returns an `Option` which will be None iff the value does not fit
-/// in `width` signed bits.
-/// 
-/// # Arguments:
-/// * `word`: An unsigned word
-/// * `width`: the width of a bit field
-/// * `lsb`: the least-significant bit of the bit field
-/// * `value`: the signed value to place into that bit field
+/// Given an unsigned 64-bit `word`, and a signed `value`,
+/// pack that `value` into `width` bits of the `word` starting at
+/// least-significant bit `lsb`, if possible.
+///
+/// # Arguments
+///
+/// * `word` - an arbitrary unsigned 64-bit word
+/// * `width` - a number of bits describing a field
+/// * `lsb` - the-least significant bit of a field
+/// * `value` - the signed value to store in the field
+///
+/// # Returns
+///
+/// an `Option<u64>` which contains the desired value at the appropriate field, if possible
+/// If the value does not fit, returns `None`
+///
 pub fn news(word: u64, width: u64, lsb: u64, value: i64) -> Option<u64> {
     Some(0)
 }
-
 
 #[cfg(test)]
 mod tests {
